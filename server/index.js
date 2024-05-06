3f062e6e2c9721a7003125da04bd9784005fe44a
Akash Singh <goandsatish@gmail.com>
2024-05-06 17:28:33 +0530

    build server using express.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("i am become mine's best of best full-stack mobile developer \n with web and block chain")
+const express = require("express")
+const http = require("http")
+
+const app = express()
+const port = process.env.port || 3000
+
+var server = http.createServer(app)
+
+var io = require("socket.io")(server)
+
+app.use(express.json())
+
+
+server.listen(port, "0.0.0.0" ,() => {
+  console.log(`Server started and running at port ${port}`)
+})
