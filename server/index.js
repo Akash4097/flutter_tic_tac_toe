5d57e0f5649e6e640760f5a385e416edf7df4c5e
Akash Singh <goandsatish@gmail.com>
2024-05-07 14:30:10 +0530

    successfully connected to mongoDB cluster.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const http = require("http")
 const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
+dotenv.config()
 
 const app = express()
 const port = process.env.port || 3000
@@ -11,11 +13,14 @@ var server = http.createServer(app)
 var io = require("socket.io")(server)
 
 app.use(express.json())
+const DB = `mongodb+srv://${process.env.MONGODB_CLUSTER_USERNAME}:${process.env.MONGODB_CLUSTER_PWD}@dev.eme9vyp.mongodb.net/?retryWrites=true&w=majority&appName=dev`
 
-// const DB = ""
+mongoose.connect(DB).then(() => {
+  console.log("MongooseDB Connection Successfull!")
+}).catch((err) => {
+  console.log(`Erro happened on mogoDB connection: ${err}`)
+})
 
-// mongoose.connect(DB)
-
-server.listen(port, "0.0.0.0" ,() => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`Server started and running at port ${port}`)
 })
