30d3431e60b2c09837410788c72e18caa9c43a8a
Akash Singh <goandsatish@gmail.com>
2024-05-07 11:20:02 +0530

    initializing mongoDB (not working now).

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const http = require("http")
+const mongoose = require("mongoose")
+
 
 const app = express()
 const port = process.env.port || 3000
@@ -10,6 +12,9 @@ var io = require("socket.io")(server)
 
 app.use(express.json())
 
+const DB = ""
+
+mongoose.connect(DB)
 
 server.listen(port, "0.0.0.0" ,() => {
   console.log(`Server started and running at port ${port}`)
